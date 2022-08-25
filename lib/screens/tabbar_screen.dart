3decb85e9d5a1254226905ecdcfa474267f5f4c2
geronimol <56521222+geronimol@users.noreply.github.com>
2022-08-25 20:32:16 -0300

    tabbar example

---
+++ lib/screens/tabbar_screen.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import '../widgets/card_widget.dart';
+
+class TabBarScreen extends StatelessWidget {
+  const TabBarScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: TabBarExample(),
+      ),
+    );
+  }
+}
+
+class TabBarExample extends StatefulWidget {
+
+  const TabBarExample({Key? key}) : super(key: key);
+
+  @override
+  State<TabBarExample> createState() => _TabBarExampleState();
+}
+
+class _TabBarExampleState extends State<TabBarExample> with SingleTickerProviderStateMixin  {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // give the tab bar a height [can change height to preferred height]
+        SizedBox(
+          height: 45,
+          child: Row(
+            children: [
+              InkWell(
+                onTap: () {},
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Icon(Icons.add, color: Colors.blue,),
+                ),
+              ),
+              Expanded(
+                child: TabBar(
+                  controller: _tabController,
+                  labelColor: Colors.black87,
+                  unselectedLabelStyle: const TextStyle(fontSize: 17),
+                  labelStyle: const TextStyle(fontWeight: FontWeight.w600, fontSize: 17),
+                  unselectedLabelColor: Colors.grey,
+                  tabs: const [
+                    // first tab [you can add an icon using the icon property]
+                    Tab(
+                      text: 'Tab 1',
+                    ),
+                    // second tab [you can add an icon using the icon property]
+                    Tab(
+                      text: 'Tab 2',
+                    ),
+                    Tab(
+                      text: 'Tab 3',
+                    ),
+                  ],
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Icon(Icons.search, color: Colors.blue,),
+                ),
+              ),
+            ],
+          ),
+        ),
+        // tab bar view here
+        Expanded(
+          child: TabBarView(
+            controller: _tabController,
+            children: const [
+              // first tab bar view widget
+              TabOne(),
+              // second tab bar view widget
+              TabTwo(),
+              // third tab bar view widget
+              TabThree(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TabThree extends StatelessWidget {
+  const TabThree({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+      crossAxisCount: 2,
+      childAspectRatio: (3/4),
+      children: const [
+        CardWidget(),
+        CardWidget(),
+        CardWidget(),
+        CardWidget(),
+        CardWidget(),
+        CardWidget(),
+        CardWidget(),
+        CardWidget(),
+      ],
+    );
+  }
+}
+
+class TabTwo extends StatelessWidget {
+  const TabTwo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+      crossAxisCount: 2,
+      childAspectRatio: (3/4),
+      children: const [
+        CardWidget(),
+        CardWidget(),
+        CardWidget(),
+      ],
+    );
+  }
+}
+
+class TabOne extends StatelessWidget {
+  const TabOne({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+      crossAxisCount: 2,
+      childAspectRatio: (3/4),
+      children: const [
+        CardWidget(),
+      ],
+    );
+  }
+}
