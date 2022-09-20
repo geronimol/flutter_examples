d3ccdf15fdcd5ff558e407ff6242ca89eaeaa9c0
Geronimo <geronimo@Macbook.local>
2022-09-20 13:08:19 -0300

    slivers

--- lib/constants.dart
+++ lib/constants.dart
@@ -12,3 +12,6 @@ const String kAlbumsUrl = 'https://jsonplaceholder.typicode.com/albums';
 const Color kPrimaryColor = Colors.blue;
 const Color kBackgroundColor = Color(0xFFF8F8F8);
 const Color kDarkGreyColor = Color(0xFF222222);
+
+/// List
+final List<String> kItemsList = List.generate(4, (index) => 'Item $index');
---
+++ lib/screens/slivers_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class SliversScreen extends StatelessWidget {
+  const SliversScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: 200,
+              stretch: true,
+              flexibleSpace: FlexibleSpaceBar(
+                stretchModes: const [StretchMode.blurBackground, StretchMode.fadeTitle, StretchMode.zoomBackground],
+                background: Stack(
+                  children: [
+                    Positioned.fill(
+                      child: Image.asset(
+                        'assets/images/test_img.jpeg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned.fill(
+                      child: Container(
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                    )
+                  ],
+                ),
+                title: const Text('Slivers'),
+              ),
+            ),
+            const SliverPadding(
+              padding: EdgeInsets.all(kDefaultPadding),
+              sliver: SliverToBoxAdapter(
+                child: Text(
+                  'Sliver List',
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20, decoration: TextDecoration.underline),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                childCount: kItemsList.length,
+                (context, index) {
+                  final item = kItemsList[index];
+                  return ListTile(
+                    contentPadding: const EdgeInsets.all(kDefaultPadding),
+                    title: Text(
+                      item,
+                      style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SliverPadding(
+              padding: EdgeInsets.all(kDefaultPadding),
+              sliver: SliverToBoxAdapter(
+                child: Text(
+                  'Sliver Grid',
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20, decoration: TextDecoration.underline),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            SliverGrid(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                childCount: kItemsList.length,
+                (context, index) {
+                  final item = kItemsList[index];
+                  return Card(
+                    elevation: 2,
+                    child: Center(
+                      child: Text(item, style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
